cb5699b542c72e57d7a0679a8b649ef197138ead
Aatish Bhatia <aatish@gmail.com>
2019-11-22 14:47:53 -0500

    Initial commit

---
+++ sketch.js
@@ -0,0 +1,149 @@
+function sketch(parent) {
+  return function( p ) { // p could be any variable name
+
+  //let countries = ['France', 'China', 'India', 'United Kingdom', 'United States', 'Chad', 'Qatar', 'Democratic Republic of Congo'];
+  //let countries = ['China', 'India', 'United Kingdom', 'United States'];
+  //let countries = ['Qatar', 'Trinidad and Tobago', 'Kuwait', 'United Arab Emirates', 'Brunei', 'Bahrain', 'Saudi Arabia']
+
+  let countries = parent.data;
+  let year = 1800;
+  let restart = parent.restart;
+
+  let dude, dudeHeight, dudeWidth;
+  let table, data = {};
+  let size, fontSize, pixelToMeters;
+  let paused  = true;
+
+  p.preload = function() {
+    table = p.loadTable('co-emissions-per-capita.csv', 'csv', 'header');
+    // https://upload.wikimedia.org/wikipedia/commons/2/2e/Black_Man_Walking_Cartoon_Vector.svg
+    dude = p.loadImage('person2.png');
+  }
+
+  p.setup = function() {
+    let canvas = p.createCanvas(700, 500);
+    canvas.parent(parent.$el);
+
+    p.frameRate(10);
+
+    p.textAlign(p.CENTER, p.CENTER);
+    p.textSize(fontSize);
+    p.imageMode(p.CENTER);
+    p.strokeWeight(2);
+
+    let rows = table.getRows();
+
+    for (let row of rows) {
+      let country = row.get('Country');
+
+      if (!(country in data)) {
+        data[country] = {};
+      }
+
+      let year = row.get('Year');
+      let co2 = row.getNum('CO2');
+
+      data[country][year] = co2;
+    }
+
+    countries = countries.sort((a,b) => data[a][2017] > data[b][2017]);
+
+    size = p.width/countries.length;
+    fontSize = p.min(0.2 * size, 40);
+
+    dudeHeight = 0.2 * dude.height;
+    dudeWidth = 0.2 * dude.width;
+
+    // say dude's height is 1.778 meters (5'10"). it's also dudeHeight pixels
+    // so conversion from meters to pixels = dudeHeight / 1.778
+    pixelToMeters = (dudeHeight) / 1.778;
+
+  }
+
+  p.draw = function() {
+
+      if (restart !== parent.restart) {
+        year = 1800;
+        restart = parent.restart;
+      }
+
+      if (countries !== parent.data) {
+        year = 1800;
+        countries = parent.data;
+
+        countries = countries.sort((a,b) => data[a][2017] > data[b][2017]);
+
+        size = p.width/countries.length;
+        fontSize = p.min(0.2 * size, 40);
+
+        dudeHeight = 0.2 * dude.height;
+        dudeWidth = 0.2 * dude.width;
+
+        // say dude's height is 1.778 meters (5'10"). it's also dudeHeight pixels
+        // so conversion from meters to pixels = dudeHeight / 1.778
+        pixelToMeters = (dudeHeight) / 1.778;
+      }
+
+      p.background('#c9d1d3');
+
+      for (let i = 0; i < countries.length; i++) {
+
+
+        p.textSize(fontSize);
+        p.textAlign(p.CENTER, p.CENTER);
+
+        let country = countries[i];
+        let xPos = p.map(i, -1, countries.length, 0, p.width);
+
+        p.text(country.replace(' ','\n'), xPos, p.height - 2*fontSize);
+
+        p.push();
+        p.translate(0,0.3 * dudeHeight);
+
+        // string
+        p.line(xPos - 0.44 * dudeWidth, p.height/2, xPos, p.height/2 - 0.9 * dudeHeight);
+
+        // dude
+        p.image(dude, xPos, p.height/2, dudeWidth, dudeHeight);
+
+        // balloon
+        p.fill('#da2d2d');
+        p.ellipse(xPos, p.height/2 - 0.9 * dudeHeight, balloonRadius(country, year) * pixelToMeters);
+        p.fill(0);
+
+        p.pop();
+
+        p.textSize(1.8 * fontSize);
+        p.textAlign(p.LEFT, p.TOP);
+        p.text(year, 0.5 * fontSize, 0.5 * fontSize);
+
+
+      }
+
+      if (year < 2017 && !paused) {
+        year++;
+      }
+  }
+
+  function balloonRadius(country, year) {
+
+    // tonnes per capita * 1000 = kg per capita
+    // kg per capita / 365 = kg per person per day
+    // kg per person per day / 1.784 kg/m^3 = m^3 per person per day
+    let balloonVolume = 1000 * data[country][year]/(1.784 * 365);
+
+    // R = (V / (4PI/3))^(1/3)
+    let balloonRadius = p.pow(balloonVolume / (4 * p.PI/3), 1/3);
+
+    return balloonRadius;
+
+  }
+
+  p.mouseClicked = function() {
+    paused = !paused;
+  }
+
+
+};
+
+}
---
+++ vue-definitions.js
@@ -0,0 +1,56 @@
+// Defines a Vue <p5> Component
+
+Vue.component('p5', {
+
+  template: '<div class="sketch"></div>',
+
+  props: ['src','data', 'restart'],
+
+  methods: {
+    // loadScript from https://stackoverflow.com/a/950146
+    // loads the p5 javscript code from a file
+    loadScript: function (url, callback)
+    {
+      // Adding the script tag to the head as suggested before
+      var head = document.head;
+      var script = document.createElement('script');
+      script.type = 'text/javascript';
+      script.src = url;
+
+      // Then bind the event to the callback function.
+      // There are several events for cross browser compatibility.
+      script.onreadystatechange = callback;
+      script.onload = callback;
+
+      // Fire the loading
+      head.appendChild(script);
+    },
+
+    loadSketch: function() {
+      this.myp5 = new p5(sketch(this));
+    }
+  },
+
+  data: function() {
+    return {
+      myp5: {}
+    }
+  },
+
+  mounted() {
+    this.loadScript(this.src, this.loadSketch);
+  }
+})
+
+// Sets up the main Vue instance
+
+var app = new Vue({
+  el: '#root',
+
+  data: {
+    restart: false,
+    selectedCountries: ['India', 'China', 'United States', 'United Kingdom'],
+    countries: ["Afghanistan", "Albania", "Algeria", "Andorra", "Angola", "Antigua and Barbuda", "Argentina", "Armenia", "Australia", "Austria", "Azerbaijan", "Bahamas", "Bahrain", "Bangladesh", "Barbados", "Belarus", "Belgium", "Belize", "Benin", "Bhutan", "Bolivia", "Bosnia and Herzegovina", "Botswana", "Brazil", "Brunei", "Bulgaria", "Burkina Faso", "Burundi", "Cambodia", "Cameroon", "Canada", "Cape Verde", "Central African Republic", "Chad", "Chile", "China", "Colombia", "Comoros", "Congo", "Costa Rica", "Cote d'Ivoire", "Croatia", "Cuba", "Cyprus", "Czech Republic", "Democratic Republic of Congo", "Denmark", "Djibouti", "Dominica", "Dominican Republic", "Ecuador", "Egypt", "El Salvador", "Equatorial Guinea", "Eritrea", "Estonia", "Ethiopia", "Fiji", "Finland", "France", "Gabon", "Gambia", "Georgia", "Germany", "Ghana", "Greece", "Grenada", "Guatemala", "Guinea", "Guinea-Bissau", "Guyana", "Haiti", "Honduras", "Hong Kong", "Hungary", "Iceland", "India", "Indonesia", "Iran", "Iraq", "Ireland", "Israel", "Italy", "Jamaica", "Japan", "Jordan", "Kazakhstan", "Kenya", "Kiribati", "Kuwait", "Kyrgyzstan", "Laos", "Latvia", "Lebanon", "Lesotho", "Liberia", "Libya", "Liechtenstein", "Lithuania", "Luxembourg", "Macedonia", "Madagascar", "Malawi", "Malaysia", "Maldives", "Mali", "Malta", "Marshall Islands", "Mauritania", "Mauritius", "Mexico", "Micronesia", "Moldova", "Mongolia", "Montenegro", "Morocco", "Mozambique", "Myanmar", "Namibia", "Nauru", "Nepal", "Netherlands", "New Zealand", "Nicaragua", "Niger", "Nigeria", "North Korea", "Norway", "Oman", "Pakistan", "Palau", "Palestine", "Panama", "Papua New Guinea", "Paraguay", "Peru", "Philippines", "Poland", "Portugal", "Qatar", "Romania", "Russia", "Rwanda", "Saint Kitts and Nevis", "Saint Lucia", "Saint Vincent and the Grenadines", "Samoa", "San Marino", "Sao Tome and Principe", "Saudi Arabia", "Senegal", "Serbia", "Seychelles", "Sierra Leone", "Singapore", "Slovakia", "Slovenia", "Solomon Islands", "Somalia", "South Africa", "South Korea", "South Sudan", "Spain", "Sri Lanka", "Sudan", "Suriname", "Swaziland", "Sweden", "Switzerland", "Syria", "Taiwan", "Tajikistan", "Tanzania", "Thailand", "Timor", "Togo", "Tonga", "Trinidad and Tobago", "Tunisia", "Turkey", "Turkmenistan", "Tuvalu", "Uganda", "Ukraine", "United Arab Emirates", "United Kingdom", "United States", "Uruguay", "Uzbekistan", "Vanuatu", "Vatican", "Venezuela", "Vietnam", "World", "Yemen", "Zambia", "Zimbabwe"]
+  }
+
+});
